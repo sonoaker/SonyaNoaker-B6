be659d3f925f99896d8594d6403f6edfbaa3f533
sonya <sonya@10.0.0.20>
2022-01-22 22:38:27 -0500

    day20 q16 hw updated

--- src/test/java/day20Hw/HwQ16.java
+++ src/test/java/day20Hw/HwQ16.java
@@ -20,9 +20,32 @@ public class HwQ16 {
 	String concatString(String strOne, String strTwo) {
 		String result = " ";
 
-		result = strOne.concat(strTwo);
+		char lastChar = strOne.charAt(strOne.length() - 1);
+		char firstChar = strTwo.charAt(0);
+		if (lastChar == firstChar) {
+			result = strOne.concat(strTwo.substring(1));
+		} else {
+			result = strOne.concat(strTwo);
+		}
 
 		return result;
 
 	}
+
+	/**
+	 * 
+	 * Write a method to concatenate two given strings such that, if the
+	 * concatenation creates a double characters then removes one of the characters.
+	 *
+	 * a. For example strOne = "food"; strTwo = "door". After concatenating two
+	 * strings it will give us "fooddoor" (dd is double character, remove one of the
+	 * d) then the string concatenation are: "fooddoor"
+	 *
+	 * Access Modifier: default Non-Access Modifier: non-static Return type: String
+	 * Method Name: concatString Parameter1: String strOne Parameter2: String strTwo
+	 *
+	 * TestData: concatString("Java","awesome") -- javawesome concatString("food",
+	 * "door") -- foodoor concatString("java","training") -- javatraining
+	 * concatString ("Pro", "ogress")-- Progress
+	 */
 }
